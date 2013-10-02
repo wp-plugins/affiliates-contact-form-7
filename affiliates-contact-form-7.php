5319ae1f21abd6cf763b987b2fe0f556fea70bb3
itthinx <itthinx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-02 16:35:39 +0000

    release 3.0.0
    git-svn-id: https://plugins.svn.wordpress.org/affiliates-contact-form-7/tags/3.0.0@781346 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ affiliates-contact-form-7.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * affiliates-contact-form-7.php
+ *
+ * Copyright (c) 2013 "kento" Karim Rahimpur www.itthinx.com
+ *
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This header and all notices must be kept intact.
+ *
+ * @author Karim Rahimpur
+ * @package affiliates-contact-form-7
+ * @since affiliates-contact-form-7 3.0.0
+ *
+ * Plugin Name: Affiliates Contact Form 7 Integration
+ * Plugin URI: http://www.itthinx.com/plugins/affiliates-contact-form-7/
+ * Description: Integrates Affiliates, Affiliates Pro and Affiliates Enterprise with Contact Form 7
+ * Author: itthinx
+ * Author URI: http://www.itthinx.com/
+ * Version: 3.0.0
+ * License: GPLv3
+ */
+if ( !defined( 'AFF_CF7_PLUGIN_DOMAIN' )) {
+	define( 'AFF_CF7_PLUGIN_DOMAIN', 'affiliates-contact-form-7' );
+}
+
+define( 'AFF_CF7_FILE', __FILE__ );
+
+include_once 'includes/class-affiliates-cf7.php';
